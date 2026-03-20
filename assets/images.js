ad25e8dab7960f1c2ed8d8a47628cb4f2ddd33de
metehankarakas <privatefethiye@gmail.com>
2026-03-20 08:25:56 +0300

    Add files via upload

---
+++ assets/images.js
@@ -0,0 +1,11 @@
+var GALLERY_IMAGES = [
+  'gallery/foto1.jpg',
+  'gallery/foto2.jpg',
+  'gallery/foto3.jpg',
+  'gallery/foto4.jpg',
+  'gallery/foto5.jpg',
+  'gallery/foto6.jpg',
+  'gallery/foto7.jpg',
+  'gallery/foto8.jpg',
+  'gallery/foto9.jpg',
+];
